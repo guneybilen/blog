80d5b76fca1479ca07dc7d9880684fe45d217099
guney bilen <guneybilen@yahoo.com>
2021-07-21 10:50:10 +0300

    mailgun configuration

--- service/sendEmailService.js
+++ service/sendEmailService.js
@@ -1,27 +1,44 @@
 // const path = require("path");
 const nodemailer = require("nodemailer");
+const mg = require("nodemailer-mailgun-transport");
 // const Email = require("email-templates");
 // const i18next = require("i18next");
 
+const mailgunAuth = {
+  auth: {
+    api_key: process.env.MAILGUN_API_KEY,
+    domain: process.env.MAILGUN_DOMAIN,
+  },
+};
+
 const sendEmailService = async (options) => {
   // console.log(options);
-  const transporter = nodemailer.createTransport({
-    host: process.env.MAIL_HOST,
-    port: process.env.MAIL_PORT,
-    auth: {
-      user: process.env.MAIL_USER,
-      pass: process.env.MAIL_PASSWORD,
-    },
-  });
+  // const transporter = nodemailer.createTransport({
+  //   host: process.env.MAIL_HOST,
+  //   port: process.env.MAIL_PORT,
+  //   auth: {
+  //     user: process.env.MAIL_USER,
+  //     pass: process.env.MAIL_PASSWORD,
+  //   },
+  // });
+
+  const transporter = nodemailer.createTransport(mg(mailgunAuth));
 
   const mailOptions = {
-    from: "admin <admin@basakblog.com>",
+    from: "admin <admin@basakblog.herokuapp.com>",
     to: options.email,
     subject: options.subject,
     text: options.message,
     html: options.messageHTML,
   };
-  await transporter.sendMail(mailOptions);
+
+  transporter.sendMail(mailOptions, function (error, response) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Successfully sent email.");
+    }
+  });
 };
 
 module.exports = sendEmailService;
